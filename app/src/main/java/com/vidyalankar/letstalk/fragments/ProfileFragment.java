a6a06d3d679ac71f4260b7673c7104284b5e42ec
Amrita284 <amrita.pawar2802@gmail.com>
2022-02-12 22:15:07 +0530

    profile design

--- app/src/main/java/com/vidyalankar/letstalk/fragments/ProfileFragment.java
+++ app/src/main/java/com/vidyalankar/letstalk/fragments/ProfileFragment.java
@@ -1,11 +1,17 @@
 package com.vidyalankar.letstalk.fragments;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,9 +19,14 @@ import android.view.ViewGroup;
 
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -23,22 +34,37 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.vidyalankar.letstalk.R;
+import com.vidyalankar.letstalk.adapter.FriendAdapter;
+import com.vidyalankar.letstalk.model.FriendModel;
 import com.vidyalankar.letstalk.model.User;
+
+
+import java.util.ArrayList;
 
 
 public class ProfileFragment extends Fragment {
 
     ImageView profilePic;
     TextView userNameTextView, userEmailTextView;
-    Button editProfile;
+    ImageView editProfile;
+
+//    Button save_profile;
+
+    public Uri imageUri;
 
     FirebaseUser user;
     DatabaseReference reference;
+    StorageReference storageRef;
 
+    ProgressBar progressBar;
     String userID;
 
-
+    RecyclerView recyclerView;
+    ArrayList<FriendModel> list;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -48,25 +74,43 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         final View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        profilePic= (ImageView) view.findViewById(R.id.profile_pic);
-        userNameTextView= (TextView) view.findViewById(R.id.profile_username);
-        userEmailTextView= (TextView) view.findViewById(R.id.profile_email);
-        editProfile= (Button) view.findViewById(R.id.edit_profile);
+        recyclerView = view.findViewById(R.id.friendRV);
 
-        user= FirebaseAuth.getInstance().getCurrentUser();
-        userID= user.getUid();
-        reference= FirebaseDatabase.getInstance().getReference("Users");
+        list = new ArrayList<>();
+
+        list.add(new FriendModel(R.drawable.profile_icon));
+        list.add(new FriendModel(R.drawable.profile_icon));
+        list.add(new FriendModel(R.drawable.profile_icon));
+        list.add(new FriendModel(R.drawable.profile_icon));
+        list.add(new FriendModel(R.drawable.profile_icon));
+
+        FriendAdapter adapter = new FriendAdapter(list, getContext());
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(adapter);
+
+
+        profilePic = (ImageView) view.findViewById(R.id.profile_user);
+        userNameTextView = (TextView) view.findViewById(R.id.profile_username);
+        userEmailTextView = (TextView) view.findViewById(R.id.profile_email);
+        editProfile = (ImageView) view.findViewById(R.id.changeProfile);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        userID = user.getUid();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        reference= FirebaseDatabase.getInstance().getReference().child("profile_pic");
+        storageRef= FirebaseStorage.getInstance().getReference().child("profile_pic").child(user.getUid());
 
         reference.child(userID).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                User userProfile= snapshot.getValue(User.class);
-                if(userProfile!=null)
-                {
-                    String userName= userProfile.username;
-                    String userEmail= userProfile.email;
+                User userProfile = snapshot.getValue(User.class);
+                if (userProfile != null) {
+                    String userName = userProfile.username;
+                    String userEmail = userProfile.email;
 
                     userNameTextView.setText(userName);
                     userEmailTextView.setText(userEmail);
@@ -82,15 +126,61 @@ public class ProfileFragment extends Fragment {
         editProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FragmentTransaction fragmentTransaction= getFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.fragmentContainerView2, new EditProfile());
-                fragmentTransaction.addToBackStack(null);
-                Toast.makeText(getActivity(), "Edit your profile...", Toast.LENGTH_LONG).show();
-                fragmentTransaction.commit();
+                mGetContent.launch("image/*");
+                save_profileImage();
             }
         });
 
         // Inflate the layout for this fragment
+
+
         return view;
     }
+        private void save_profileImage() {
+
+            progressBar.setVisibility(View.VISIBLE);
+
+            StorageReference fileRef= storageRef.child(user.getUid() + ".jpg");
+            fileRef.putFile(imageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                    if(task.isSuccessful())
+                    {
+                        fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                FirebaseDatabase.getInstance().getReference().child("Users").child(user.getUid()).child("profilePic").setValue(uri.toString());
+                                progressBar.setVisibility(View.GONE);
+
+                                Toast.makeText(getActivity(), "Image uploaded", Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    }else {
+                        progressBar.setVisibility(View.GONE);
+                        Toast.makeText(getActivity(),  task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    progressBar.setVisibility(View.GONE);
+                    Toast.makeText(getActivity(),"Failed! "+ e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+
+        }
+
+
+
+        ActivityResultLauncher<String> mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+            @Override
+            public void onActivityResult(Uri result) {
+                if(result!= null)
+                {
+                    editProfile.setImageURI(result);
+                    imageUri= result;
+                }
+            }
+        });
+
 }
