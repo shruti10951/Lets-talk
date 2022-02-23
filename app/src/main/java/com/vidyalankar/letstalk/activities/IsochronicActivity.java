750b148df4f434c88e05d1e6e883f9e8f253b703
Amrita284 <amrita.pawar2802@gmail.com>
2022-02-24 00:09:41 +0530

    Melodies Finished

--- app/src/main/java/com/vidyalankar/letstalk/activities/IsochronicActivity.java
+++ app/src/main/java/com/vidyalankar/letstalk/activities/IsochronicActivity.java
@@ -1,28 +1,19 @@
 package com.vidyalankar.letstalk.activities;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.media.MediaPlayer;
-import android.media.PlaybackParams;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Handler;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.vidyalankar.letstalk.R;
 
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
-
 public class IsochronicActivity extends AppCompatActivity {
 
     String songName;
-    TextView song1, song2, song3, song4;
+    TextView binaural1, binaural2, binaural3, binaural4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,29 +24,29 @@ public class IsochronicActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 switch (view.getId()){
-                    case R.id.song1:
+                    case R.id.binaural1:
                         songName= "song 1";
                         forward();
                         break;
-                    case R.id.song2:
+                    case R.id.binaural2:
                         songName= "song 2";
                         forward();
                         break;
-                    case R.id.song3:
+                    case R.id.binaural3:
                         songName= "song 3";
                         forward();
                         break;
-                    case R.id.song4:
+                    case R.id.binaural4:
                         songName= "song 4";
                         forward();
                         break;
                 }
             }
         };
-        findViewById(R.id.song1).setOnClickListener(listener);
-        findViewById(R.id.song2).setOnClickListener(listener);
-        findViewById(R.id.song3).setOnClickListener(listener);
-        findViewById(R.id.song4).setOnClickListener(listener);
+        findViewById(R.id.binaural1).setOnClickListener(listener);
+        findViewById(R.id.binaural2).setOnClickListener(listener);
+        findViewById(R.id.binaural3).setOnClickListener(listener);
+        findViewById(R.id.binaural4).setOnClickListener(listener);
 
 
     }
@@ -63,7 +54,7 @@ public class IsochronicActivity extends AppCompatActivity {
 
     private void forward() {
 
-        Intent intent= new Intent(IsochronicActivity.this, PlayMelodies.class);
+        Intent intent= new Intent(IsochronicActivity.this, PlayMelodies2.class);
         intent.putExtra("songName", songName);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
